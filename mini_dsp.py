4c41bfa7cb5c6e508b2ad37520a24b0e5daeec4f
Tomek Horszczaruk <tkhorszczaruk@gmail.com>
2021-02-20 15:52:38 +0100

    Add chunk labels

--- mini_dsp.py
+++ mini_dsp.py
@@ -10,12 +10,14 @@ p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paInt16, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
 player = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, output=True, frames_per_buffer=CHUNK)
 
+i = 1
+
 # Read signals forever
 while True:
     try:
         data = np.frombuffer(stream.read(CHUNK), dtype=np.int16)
-        # player.write(data * 10, CHUNK)
-        print(data[:7])
+        print(f'Chunk no. {i}: ' + str(data[:7]))
+        i += 1
         
     except KeyboardInterrupt:
         break
